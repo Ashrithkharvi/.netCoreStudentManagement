406f82415c75e8b2248411a96d66dc2d3da47f47
Ashrithkharvi <kharviashrith06@gmail.com>
2024-11-07 09:50:15 +0530

    Update Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -3,8 +3,8 @@
     public class Category
     {
         public int Id { get; set; }
-        public required string Name { get; set; } // or string? Name { get; set; }
-        public ICollection<Student>? Students { get; set; } // or ICollection<Student> Students { get; set; } = new List<Student>();
+        public required string Name { get; set; } 
+        public ICollection<Student>? Students { get; set; }
     }
 
 
